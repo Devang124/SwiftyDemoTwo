59e1b045b5339f5a6c9df3d2a4cdceb61b97561b
devang.parmar <devang.parmar@volansys.com>
2024-03-01 12:08:35 +0530

    Initial project setup

---
+++ SwiftyDemoTwo/SwiftyDemoTwo.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyDemoTwo.h
+//  SwiftyDemoTwo
+//
+//  Created by Devang Parmar on 01/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyDemoTwo.
+FOUNDATION_EXPORT double SwiftyDemoTwoVersionNumber;
+
+//! Project version string for SwiftyDemoTwo.
+FOUNDATION_EXPORT const unsigned char SwiftyDemoTwoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyDemoTwo/PublicHeader.h>
+
+
---
+++ SwiftyDemoTwoExample/SwiftyDemoTwoExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyDemoTwoExampleApp.swift
+//  SwiftyDemoTwoExample
+//
+//  Created by Devang Parmar on 01/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyDemoTwoExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
