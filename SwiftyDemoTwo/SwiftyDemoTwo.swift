6340757b438bf5bafa44a01ec61140a670d73fbd
devang.parmar <devang.parmar@volansys.com>
2024-03-01 12:49:21 +0530

    Added test cases

---
+++ SwiftyDemoTwo/SwiftyDemoTwo.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftyDemoTwo.swift
+//  SwiftyDemoTwo
+//
+//  Created by Devang Parmar on 01/03/24.
+//
+
+import Foundation
+
+public final class SwiftyDemoTwo {
+
+    let name = "SwiftyDemoTwo"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- SwiftyDemoTwoTests/SwiftyDemoTwoTests.swift
+++ SwiftyDemoTwoTests/SwiftyDemoTwoTests.swift
@@ -9,15 +9,26 @@ import XCTest
 @testable import SwiftyDemoTwo
 
 final class SwiftyDemoTwoTests: XCTestCase {
-
+    
+    var swiftyDemoTwo : SwiftyDemoTwo!
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        swiftyDemoTwo = SwiftyDemoTwo()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
+    func testAdd() {
+            XCTAssertEqual(swiftyDemoTwo.add(a: 1, b: 1), 2)
+    }
+    
+    func testSub() {
+            XCTAssertEqual(swiftyDemoTwo.sub(a: 2, b: 1), 1)
+    }
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
